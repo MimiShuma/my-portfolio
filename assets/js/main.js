d454fb60cf5e732be509c7eaf434495e6030a58f
It's Mimi <shuma.ttt@gmail.com>
2024-04-28 15:53:29 +0600

    navbar and hero section added

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Frontend Designer", "Web Developer", "Graphics Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
